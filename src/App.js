64dc7b7a5abe9f3e5ab05475d6c291b07f11e724
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-25 10:36:26 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef(null);
+
+  const options = {
+    smooth: true,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocomotiveScrollProvider options={options} containerRef={ref}>
+      <main data-scroll-container ref={ref}>
+        <section className="intro" data-scroll-section>
+          <h1>This is the Introduction section</h1>
+        </section>
+
+        <section className="contents" data-scroll-section>
+          <h1>I Love React</h1>
+        </section>
+
+        <section id="stick" data-scroll-section>
+          <h1>Hey I'm Sticky</h1>
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+        </section>
+
+        <section className="footer">
+          <h1>Let's end the application with this Footer</h1>
+        </section>
+      </main>
+    </LocomotiveScrollProvider>
   );
 }
 
